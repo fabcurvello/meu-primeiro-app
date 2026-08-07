489b1a1dacf89bf46f0d326feb315ce780b33bbf
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2026-08-07 10:11:21 -0300

    Signals carrinho

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -8,12 +8,20 @@ import { Produto } from '../produto/produto';
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
+  // SIGNALS
+
   // writable signal -> signal (reativo) que permite alterações (com set ou update)
   produtos = signal([
     { nome: 'Notebook', preco: 3800 },
     { nome: 'Mouse', preco: 150 },
     { nome: 'Fone', preco: 80 },
   ]);
+
+  produtoSelecionado = signal<string | null>(null);
+
+  carrinho = signal<{ nome: string; preco: number }[]>([]);
+
+  // COMPUTED SIGNALS
 
   // computed signal -> observa outro signal e se atualiza automaticamente
   totalProdutos = computed(() => this.produtos().length);
@@ -22,22 +30,13 @@ export class ListaProdutos {
     return this.produtos().reduce((total, item) => total + item.preco, 0);
   });
 
-  exibirProduto(nome: string) {
-    this.produtoSelecionado.set(nome);
-  }
+  quantidadeCarrinho = computed(() => this.carrinho().length);
 
-  produtoSelecionado = signal<string | null>(null);
+  totalCarrinho = computed(() => {
+    return this.carrinho().reduce((total, item) => total + item.preco, 0);
+  });
 
-  // update -> adiciona um item ao writable signal
-  adicionarProduto() {
-    this.produtos.update((listaAtual) => [...listaAtual, { nome: 'Teclado', preco: 250 }]);
-  }
-
-  // set -> altera um item do writable signal
-  substituirProdutos() {
-    this.produtos.set([{ nome: 'Produto novo', preco: 999 }]);
-  }
-
+  // EFFECTS
   // método construtor - formata os objetos criados a partir desta classe
   constructor() {
     // estes 2 effects geram mensagens no terminal sempre que alterações são realizadas.
@@ -50,7 +49,7 @@ export class ListaProdutos {
     effect(() => {
       console.log('Valor total atualizado:', this.valorTotal());
     });
-    
+
     // effect observa o title da página e altera se a condição for atendida
     effect(() => {
       if (typeof document !== 'undefined') {
@@ -58,4 +57,24 @@ export class ListaProdutos {
       }
     });
   } // fim do constructor
+
+  // AÇÕES QUE ALTERAM VALORES DE SIGNALS (SET E UPDATE)
+
+  exibirProduto(nome: string) {
+    this.produtoSelecionado.set(nome);
+  }
+
+  // update -> adiciona um item ao writable signal
+  adicionarProduto() {
+    this.produtos.update((listaAtual) => [...listaAtual, { nome: 'Teclado', preco: 250 }]);
+  }
+
+  // set -> altera um item do writable signal
+  substituirProdutos() {
+    this.produtos.set([{ nome: 'Produto novo', preco: 999 }]);
+  }
+
+  adicionarAoCarrinho(produto: { nome: string; preco: number }) {
+    this.carrinho.update((listaAtual) => [...listaAtual, produto]);
+  }
 }
