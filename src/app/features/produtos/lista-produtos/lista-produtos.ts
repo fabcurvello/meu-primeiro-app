3ba3cdd7a546e4eebc4f90c8f9ed4a4b1bf1c647
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2026-08-14 09:54:37 -0300

    Angular Material

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,11 +1,12 @@
 import { Component, signal, computed, effect, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 import { Produto } from '../produto/produto';
 import { ProdutosService } from '../produtos.service';
 
 @Component({
   selector: 'app-lista-produtos',
-  imports: [Produto],
+  imports: [Produto, MatButtonModule],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
